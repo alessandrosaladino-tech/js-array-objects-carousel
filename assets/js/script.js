70c6d4591171f38521ea314e91fbae335da81fbf
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-07 17:15:25 +0200

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,7 @@
 Riprendiamo il live coding visto in classe un pó di tempo fá sul carosello di immagini e rifacciamolo :gatto_che_urla:, questa volta usando gli oggetti.
 :boolean-hug: Potete prendere come riferimento il codice scritto insieme nel live, che troverete direttamente nella mia repository di github a questo link: [https://github.com/fabiopacifici/104_js/tree/main/live_slider]
 Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto di un array di stringhe.
-Bonus 0:
-Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le funzioni. É possibile fare refactoring del codice, pulendolo e creando quanche funzione che possa rendere tutto piú leggibile e pulito?
+
 
 
 
@@ -100,7 +99,7 @@ nextEl.addEventListener('click', function () {
 
     // activeSlide = 4
 
-    if (activeSlide === slidesImages.length - 1) {
+    if (activeSlide === slidesImages.length - 1) {                
         activeSlide = 0
         // activeSlide = 5
     } else {
