c891724d5d52326972bb9a09d52704cbf5d7466c
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-07 17:12:37 +0200

    Add
    thumbnails

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,143 +22,140 @@ Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le fun
 
 
 
-/* Define the slides list */
+// Creo un array di oggetti contenete le immagini (oggetti) **CAMBIATO DA ME**
 const slides = [
     {
         image: './assets/img/01.webp'
-    }, 
+    },
 
     {
         image: './assets/img/02.webp'
     },
 
     {
-        image:'./assets/img/03.webp'
+        image: './assets/img/03.webp'
     },
 
     {
-        image:'./assets/img/04.webp'
+        image: './assets/img/04.webp'
     },
 
     {
-        image:'./assets/img/05.webp'
+        image: './assets/img/05.webp'
     },
-  ]
-  
-  let activeSlide = 0;
-  
-  // select the dom elements
-  const sliderImagesEl = document.querySelector('.slider .images')
-  const prevEl = document.querySelector('.prev')
-  const nextEl = document.querySelector('.next')
-  
-  
+]
 
-  
-  //Stampo le immagini nel DOM
-  const imageMarkup = slides.map ((element, i)=> {
-    console.log(element);
-       // for each slide we create the markup
-       const slideMarkup = `<img class="${activeSlide === i ? 'active' : '' }" src="${element.image}" alt="">`
-       //console.log(slideMarkup);
-     
-       sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
-  })
-  
-  
-  const thumbsElement = document.querySelector('.thumbnails')
-  
-  
+let activeSlide = 0;
 
-  const thimbPath = slides.map ((element, i)=> {
+// select the dom elements
+const sliderImagesEl = document.querySelector('.slider .images')
+const prevEl = document.querySelector('.prev')
+const nextEl = document.querySelector('.next')
+
+
+
+
+//Stampo le immagini nel DOM  **CAMBIATO DA ME**
+const imageMarkup = slides.map((element, i) => {
     console.log(element);
-       // for each slide we create the markup
-       const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="${element.image}" alt="">`
-       //console.log(slideMarkup);
-     
-       thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
-  })
-  
-  
-  
-  
-  
-  // intercept click on the next icon 
-  nextEl.addEventListener('click', function(){
+    // creo markup per ogni immagine 
+    const slideMarkup = `<img class="${activeSlide === i ? 'active' : ''}" src="${element.image}" alt="">`
+
+    sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
+})
+
+
+const thumbsElement = document.querySelector('.thumbnails')
+
+
+
+// Creo markup per ogni immagine per la thumbnails  **CAMBIATO DA ME**
+const thimbPath = slides.map((element, i) => {
+    console.log(element);
+    const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="${element.image}" alt="">`
+    thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
+})
+
+
+const slidesImages = document.querySelectorAll('.slider .images > img')
+console.log(slidesImages);
+
+
+// intercept click on the next icon 
+nextEl.addEventListener('click', function () {
     console.log('cliccato su next');
-  
+
     // select the current slide
     const currentSlide = slidesImages[activeSlide]
     console.log(currentSlide);
     // remove the active class from the current slide
     currentSlide.classList.remove('active')
-  
+
     // select the active thumb
     const currentThumb = document.querySelector('.thumbnails > img.active')
     console.log(currentThumb);
     // remove the active class from the active thumb
     currentThumb.classList.remove('active')
-  
-    
+
+
     // activeSlide = 4
-  
+
     if (activeSlide === slidesImages.length - 1) {
-      activeSlide = 0
-      // activeSlide = 5
+        activeSlide = 0
+        // activeSlide = 5
     } else {
-      // increment the activeSlide of 1
-      activeSlide++
+        // increment the activeSlide of 1
+        activeSlide++
     }
-  
-  
+
+
     // select the next slide
     const nextSlide = slidesImages[activeSlide]
     console.log(nextSlide);
     // add the active class to the next slide
     nextSlide.classList.add('active')
-  
-  
-    /* TODO */
-  
-  
+
+
+
+
     // select the next thumb
     const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
     console.log(nextThumb);
     // add to the next thumb the active class
     nextThumb.classList.add('active')
-  
-  
-  })
-  
-  // intercept click on the prev icon
-  
-  
-  // activeSlide = 0
-  prevEl.addEventListener('click', function () {
+
+
+})
+
+// intercept click on the prev icon
+
+
+// activeSlide = 0
+prevEl.addEventListener('click', function () {
     console.log('cliccato su prev');
-  
-  
+
+
     // select the current slide
     const currentSlide = slidesImages[activeSlide]
     console.log(currentSlide);
     // remove the active class from the current slide
     currentSlide.classList.remove('active')
-  
+
     if (activeSlide === 0) {
-      activeSlide = slidesImages.length - 1
-      // activeSlide = 5
+        activeSlide = slidesImages.length - 1
+        // activeSlide = 5
     } else {
         // decrement the activeSlide of 1
         activeSlide--
     }
-    
-    
+
+
     console.log(activeSlide);
-  
-  
+
+
     // select the next slide
     const nextSlide = slidesImages[activeSlide]
     console.log(nextSlide);
     // add the active class to the next slide
     nextSlide.classList.add('active')
-  })
+})
